02cbec367fe4d5970e56d6348c221bf37b184075
Liu Siyuan <weifengzi2009@gmail.com>
2016-06-12 00:42:40 +0800

    add 1.2 ~ 1.5

---
+++ chapter1/1.3.scm
@@ -0,0 +1,13 @@
+; define square procedure
+(define (square x)
+  (* x x))
+
+; define sum of square procedure
+(define (sum-of-square x y)
+  (+ (square x) (square y)))
+
+; define sum of square of larger two numbers procedure
+(define (sum-of-square-larger-two x y z)
+  (cond ((and (<= x y) (<= x z)) (sum-of-square y z))
+        ((and (<= y x) (<= y z)) (sum-of-square x z))
+        ((and (<= z x) (<= z y)) (sum-of-square z y))))
